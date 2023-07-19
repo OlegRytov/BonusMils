0b17f120795a0e9bf26715be44d078b8eb2dc7c8
Oleg Rytov <cattimofei@gmail.com>
2023-07-19 19:19:19 +0300

    add BonusMiles

---
+++ src/BonusMiles.java
@@ -0,0 +1,8 @@
+public class BonusMiles {
+    public static void main(String[] args) {
+        int ticketPrice = 24543;  // стоимость билета
+        int mileСost = 20; // стоимость мили
+        System.out.println("Количество бонусных миль = " + ticketPrice / mileСost);
+    }
+
+}
