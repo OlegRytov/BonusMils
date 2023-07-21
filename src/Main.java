4c2f57b21e4a9804661179e3d9534e29a5ae7c6b
Oleg Rytov <cattimofei@gmail.com>
2023-07-21 11:02:45 +0300

    Rename Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class BonusMiles {
+public class Main{
     public static void main(String[] args) {
         int ticketPrice = 24543;  // стоимость билета
         int mileСost = 20; // стоимость мили
